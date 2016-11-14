e8368ba18af6b11138f983f763d36988101b36ba
iamkenloh <iamkenloh@gmail.com>
2016-11-14 14:26:26 +0000

    Stable build with UI Routing and Rest Service

--- ExsirsizeParent/ExSirSizeUI/src/main/resources/public/js/essApp.js
+++ ExsirsizeParent/ExSirSizeUI/src/main/resources/public/js/essApp.js
@@ -4,6 +4,30 @@ app.controller("homeController", function($scope, UserService){
     $scope.text = UserService.userName;
 });
 
+app.controller("mainController", function($scope, UserService){
+
+});
+
+app.controller("settingsController", function($scope, UserService){
+
+});
+
+app.controller("loginController", function($scope, $location, UserService){
+    var vm = this;
+    vm.dataLoading = false;
+    vm.errorMessage = '';
+
+    vm.login = function(){
+        if(vm.username === "iamkenloh"){
+            UserService.isLogged = true;
+            $location.path('/home');
+        } else {
+            vm.errorMessage = 'incorrect login';
+        }
+    };
+
+});
+
 /* Object User Service returns some data that can be used in different controllers*/
 app.factory('UserService', function(){
     return {
@@ -16,17 +40,24 @@ app.factory('UserService', function(){
 app.config(function($routeProvider){
     $routeProvider
         .when("/", {
-            templateUrl : "views/main.html"
+            templateUrl : "views/main.html",
+            controller: "mainController as vm"
         })
         .when("/home", {
             templateUrl : "views/home.html",
-            controller : "homeController as hc"
+            controller : "homeController as vm"
         })
         .when("/settings", {
-            templateUrl : "views/settings.html"
+            templateUrl : "views/settings.html",
+            controller: "settingsController as vm"
+        })
+        .when("/login", {
+            templateUrl:"views/login.html",
+            controller: "loginController as vm"
         })
         .otherwise({
-            template : "<h1>I'll be surprised if this works</h1>"
+            templateUrl : "views/main.html",
+            controller: "mainController as vmßß"
         });
 });
 
