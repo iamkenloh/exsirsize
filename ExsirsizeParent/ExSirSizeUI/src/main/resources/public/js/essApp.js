648a7bf5503ae6e5f4ff74fb1b0ea1b73d5093b8
iamkenloh <iamkenloh@gmail.com>
2016-11-14 13:34:33 +0000

    Stable build with UI Routing and Rest Service

--- ExsirsizeParent/ExSirSizeUI/src/main/resources/public/js/essApp.js
+++ ExsirsizeParent/ExSirSizeUI/src/main/resources/public/js/essApp.js
@@ -1,19 +1,26 @@
 var app = angular.module("essApp", ['ngRoute']);
 
-app.controller("myCtrl", function(){
-    this.id = "Ken";
-
-    console.log("I am definitely here");
+app.controller("homeController", function($scope, UserService){
+    $scope.text = UserService.userName;
 });
+
+/* Object User Service returns some data that can be used in different controllers*/
+app.factory('UserService', function(){
+    return {
+        isLogged : false,
+        userName : ", A Whole Bunch Of Text DUDE"
+    };
+});
+
 
 app.config(function($routeProvider){
     $routeProvider
         .when("/", {
-            templateUrl : "views/main.html",
-            controller : "myCtrl"
+            templateUrl : "views/main.html"
         })
         .when("/home", {
-            templateUrl : "views/home.html"
+            templateUrl : "views/home.html",
+            controller : "homeController as hc"
         })
         .when("/settings", {
             templateUrl : "views/settings.html"
@@ -22,3 +29,4 @@ app.config(function($routeProvider){
             template : "<h1>I'll be surprised if this works</h1>"
         });
 });
+
