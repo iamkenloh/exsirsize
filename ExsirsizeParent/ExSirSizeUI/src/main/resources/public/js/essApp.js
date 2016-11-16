621547c782c1caea7d6062d26f41e495a75b3ca1
iamkenloh <iamkenloh@gmail.com>
2016-11-16 17:14:38 +0000

    Story-1 Able to Login

--- ExsirsizeParent/ExSirSizeUI/src/main/resources/public/js/essApp.js
+++ ExsirsizeParent/ExSirSizeUI/src/main/resources/public/js/essApp.js
@@ -1,4 +1,4 @@
-var app = angular.module("essApp", ['ngRoute']);
+var app = angular.module("essApp", ['ngRoute']).run(run);
 
 app.controller("homeController", function($scope, UserService){
     $scope.text = UserService.userName;
@@ -22,8 +22,9 @@ app.controller("loginController", function($scope, $location, UserService){
     vm.errorMessage = '';
 
     vm.login = function(){
-        if(vm.username === "iamkenloh"){
+        if(vm.username === "iamkenloh" || vm.username === "meziskeen"){
             UserService.isLogged = true;
+            UserService.userName = vm.username;
             $location.path('/home');
         } else {
             vm.errorMessage = 'incorrect login';
@@ -45,28 +46,71 @@ app.config(function($routeProvider){
     $routeProvider
         .when("/", {
             templateUrl : "views/main.html",
-            controller: "mainController as vm"
+            controller: "mainController as vm",
+            requiresLogin : false
         })
         .when("/home", {
             templateUrl : "views/home.html",
-            controller : "homeController as vm"
+            controller : "homeController as vm",
+            requiresLogin : true
         })
         .when("/workout", {
             templateUrl : "views/home.html",
-            controller : "workoutController as vm"
+            controller : "workoutController as vm",
+            requiresLogin : true
         })
         .when("/settings", {
             templateUrl : "views/settings.html",
-            controller: "settingsController as vm"
+            controller: "settingsController as vm",
+            requiresLogin : true
         })
         .when("/login", {
             templateUrl:"views/login.html",
-            controller: "loginController as vm"
+            controller: "loginController as vm",
+            requiresLogin : false
         })
         .otherwise({
             templateUrl : "views/main.html",
-            controller: "mainController as vmßß"
+            controller: "mainController as vm",
+            requiresLogin : false
         });
 });
+
+run.$inject = ['$rootScope', '$location', '$route','UserService'];
+function run($rootScope, $location,$route, UserService) {
+    // keep user logged in after page refresh
+//    $rootScope.globals = $cookieStore.get('globals') || {};
+//    if ($rootScope.globals.currentUser) {
+//        $http.defaults.headers.common['Authorization'] = 'Basic ' + $rootScope.globals.currentUser.authdata; // jshint ignore:line
+//    }
+
+
+    $rootScope.$on('$locationChangeStart', function (event, next, current) {
+        // redirect to login page if not logged in and trying to access a restricted page
+//        var restrictedPage = $.inArray($location.path(), ['/login', '/register']) === -1;
+//        var loggedIn = $rootScope.globals.currentUser;
+        var nextPath = $location.path();
+        var nextRoute = $route.routes[nextPath];
+        if (!UserService.isLogged && nextRoute.requiresLogin) {
+            $location.path('/login');
+        }
+    });
+};
+
+//app.directive("checkUser",['$rootScope','$location','UserService', function ($rootScope,$location,UserService) {
+//        return {
+//            link: function (scope, element, attrs) {
+//                console.log("ROUTE CHANGE");
+//                $rootScope.$on('$routeChangeStart', function(event, next, current){
+//
+//                    console.log("ROUTE CHANGE");
+//                    if (!UserService.isLogged) {
+//                        $location.path('/login');
+//                    }
+//                });
+//            }
+//        }
+//    }]
+//);
 
 
